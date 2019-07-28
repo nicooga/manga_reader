8f70ba989cee0ec1f604c9ef7f1bbb692bf87eab
Nicolas Oga <2112.oga@gmail.com>
2019-07-28 18:53:47 -0300

    Put our code for initializing app state in a better place

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import 'normalize.css'
 
-import React  from 'react'
+import React, { useLayoutEffect }  from 'react'
 import styled from 'styled-components'
 
 import { useAppState } from './StateManagement'
@@ -9,14 +9,14 @@ import ApolloProvider from './ApolloProvider'
 import AuthPage from './pages/AuthPage'
 import LogoutButton from './LogoutButton'
 
-initAuthState()
-
 const Root = styled.div`
   font-family: 'Helvetica';
 `
 
 const App = _props => {
   const currentUser = useAppState('currentUser')
+
+  useLayoutEffect(_ => { initAuthState() })
 
   return (
     <ApolloProvider>
