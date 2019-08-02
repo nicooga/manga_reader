17ae023b84c36ca35d2aa80f60c0dc779cd7bbce
Nicolas Oga <2112.oga@gmail.com>
2019-08-02 12:56:38 -0300

    Solve problem with currentUser being "undefined" which is a truthy value

--- front/src/Auth.js
+++ front/src/Auth.js
@@ -2,6 +2,6 @@ import ls from 'local-storage'
 import stateManager from './stateManager'
 
 const setCurrentUser = ({ user, token }) => stateManager.updateState({ currentUser: user, token })
-const logout = _ => stateManager.updateState({ currentUser: undefined, token: undefined })
+const logout = _ => stateManager.updateState({ currentUser: null, token: null })
 
 export { setCurrentUser, logout }
